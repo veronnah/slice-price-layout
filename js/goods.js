55f21dbb5775b3c4d8627a253dde3670d63e87a1
veronnah <yoo.veroon@gmail.com>
2021-05-01 15:34:25 +0200

    goods page: inputs added, input number

--- js/goods.js
+++ js/goods.js
@@ -20,3 +20,15 @@
 	hamburger.initialize();
 
 }());
+
+//input number	
+var spins = document.getElementsByClassName("spin");
+for (var i = 0, len = spins.length; i < len; i++) {
+    var spin = spins[i],
+        span = spin.getElementsByTagName("span"),
+        input = spin.getElementsByTagName("input")[0];
+    
+    input.onchange = function() { input.value = +input.value || 0; };
+    span[0].onclick = function() { input.value = Math.max(0, input.value - 1); };
+    span[1].onclick = function() { input.value -= -1; };
+}
