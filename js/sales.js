527aac63bde6b6c9c18b9cee2d19bb66c8e5ef05
veronnah <yoo.veroon@gmail.com>
2021-05-06 12:03:03 +0200

    sales adaptation

--- js/sales.js
+++ js/sales.js
@@ -115,3 +115,25 @@ $('.month-slider-wrapper .shortcuts li').mousedown(function () {
 getIndex(2);
 
 
+(function() {
+
+	let hamburger = {
+		nav: document.querySelector('#nav'),
+		navToggle: document.querySelector('.nav-toggle'),
+        navWrapper: document.querySelector('.nav-toggle__wrap'),
+
+		initialize() {
+			this.navToggle.addEventListener('click',
+        () => { this.toggle(); });
+		},
+
+		toggle() {
+			this.navToggle.classList.toggle('expanded');
+			this.nav.classList.toggle('expanded');
+            this.navWrapper.classList.toggle('expanded');
+		},
+	};
+
+	hamburger.initialize();
+
+}());
